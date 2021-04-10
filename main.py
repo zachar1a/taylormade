e3f586ca8423be84ad68a6ca80089d8c61497387
Zachary Gonzalez <zacharycg4@gmail.com>
2021-04-09 20:53:58 -0500

    using driver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests, json
 import urllib.request
 from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 
 
@@ -9,9 +10,12 @@ from bs4 import BeautifulSoup as bs
 # that they won't detect that we are using an automated
 # service
 
-url = 'https://stockx.com/adidas-yeezy-450-cloud-white'
+url = 'https://stockx.com/buy/adidas-yeezy-450-cloud-white'
 #url = 'http://worldagnetwork.com/'
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_3) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15'}
-result = requests.get(url, headers=headers)
-print(result.text)
 
+result = requests.get(url, headers=headers)
+soup = BeautifulSoup(result.text, 'html.parser')
+
+allP = soup.find_all('div')
+print(allP)
